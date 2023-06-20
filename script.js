7b96ed439dc53283f01f2dafb527a79b40cfaf8e
Leckller <7leckler@gmail.com>
2023-06-20 13:51:05 -0300

    feat: first step

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 const corpo = document.querySelector('body');
+const cabecalhoCreate = document.createElement('header');
+const tituloCreate = document.createElement('h1');
+corpo.appendChild(cabecalhoCreate);
+document.querySelector('header').id = 'cabecalho';
+const cabecalho = document.querySelector('#cabecalho');
+cabecalho.appendChild(tituloCreate);
+const h1Cabecalho = document.querySelector('h1');
+h1Cabecalho.id = 'title';
+h1Cabecalho.innerText = "Paleta de Cores";
+const principal = document.createElement('main');
+const palheta = document.createElement('ul');
+corpo.appendChild(principal);
+const mainLocal = document.querySelector('main');
+const secao = document.createElement('section');
+mainLocal.appendChild(secao);
+const sectionLocal = document.querySelector('section');
+for (let i = 0; i <= 3; i += 1) {
+  const div = document.createElement('div');
+  div.classList.add('color');
+  sectionLocal.appendChild(div);
+}
+sectionLocal.firstElementChild.classList.add('black');
+sectionLocal.firstChild.nextSibling.classList.add('red');
+sectionLocal.lastElementChild.previousSibling.classList.add('blue');
+sectionLocal.lastElementChild.classList.add('green');
 
-window.onload
+document.querySelector('section').id = 'color-palette';
+mainLocal.appendChild(palheta);
+mainLocal.style.width = '600px';
+mainLocal.style.height = '600px';
+
+window.onload = () => {
+};
